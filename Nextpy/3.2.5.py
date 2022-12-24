85c6ce20d4df94d06cd4ca088726f947488d07fe
Eitan <eitanlich@yahoo.com>
2022-12-24 18:23:15 +0200

    Exceptions

---
+++ Nextpy/3.2.5.py
@@ -0,0 +1,16 @@
+def read_file(file_name):
+    try:
+        content = "__CONTENT_START__\nr\n__CONTENT_END__"
+        f = open(file_name, "r")
+    except FileNotFoundError:
+        cont = content.replace("r", "__NO_SUCH_FILE__")
+    else:
+        cont = content.replace("r", f.read())
+        f.close()
+    finally:
+        print(cont)
+
+
+
+
+read_file("name_length.txtt")
