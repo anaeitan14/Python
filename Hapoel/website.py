a33881df0814315dc68aaf3fe674af5fbf2108da
Eitan <eitanlich@yahoo.com>
2023-01-01 12:49:05 +0200

    Hapoel

--- Hapoel/website.py
+++ Hapoel/website.py
@@ -3,11 +3,8 @@ import csv
 def read_info():
     f = open(r"files/players.csv", 'r')
     reader = csv.reader(f)
-
     players = {}
-
     fields = next(reader)
-
 
     """ saving the info of each player as a list 
     
@@ -30,6 +27,7 @@ def make_html(player_info, fields):
     html_file = open(r"files/hb7/template.htm", "r", encoding="utf-8")
     html_text = html_file.read()
     html_file.close()
+
 
     for player in player_info.keys():
         f = open(f"files/hb7/{player}.htm", "w", encoding="utf-8")
