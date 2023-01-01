f9971ad8098c9f8619117a2e898f2f25d7efe8e4
Eitan <eitanlich@yahoo.com>
2023-01-01 12:25:47 +0200

    Hapoel

--- Hapoel/website.py
+++ Hapoel/website.py
@@ -8,31 +8,37 @@ def read_info():
 
     fields = next(reader)
 
+
+    """ saving the info of each player as a list 
+    
     for line in reader:
         players[line[-1]] = line
+    """
 
+    for player in reader:
+        players[player[-1]] = {}
+        index = 0
+        for field in fields:
+            players[player[-1]][field] = player[index]
+            index+=1
     print(players)
-
     f.close()
 
-    return players
+    return players, fields
 
-def make_html(player_info):
+def make_html(player_info, fields):
     html_file = open(r"files/hb7/template.htm", "r", encoding="utf-8")
     html_text = html_file.read()
     html_file.close()
 
-    for player in player_info.items():
-        f = open(f"files/hb7/{player[0]}.htm", "w", encoding="utf-8")
+    for player in player_info.keys():
+        f = open(f"files/hb7/{player}.htm", "w", encoding="utf-8")
         player_html = html_text
-        player_html = player_html.replace("%%name%%", player[1][1])
-        player_html = player_html.replace("%%birth%%", player[1][0])
-        player_html = player_html.replace("%%number%%", player[1][2])
-        player_html = player_html.replace("%%position%%", player[1][-2])
-        player_html = player_html.replace("%%id%%", player[1][-1])
+        for field in fields:
+            player_html = player_html.replace(f"%%{field}%%", player_info[player][field])
         f.write(player_html)
         f.close()
 
 if __name__ == "__main__":
-    players_info = read_info()
-    make_html(players_info)
+    players_info, fields = read_info()
+    make_html(players_info, fields)
